0eeebae9bde1abe04a8a03e1c99014e08012233a
blackparadise0407 <blackparadise0407@gmail.com>
2023-04-26 14:12:39 +0700

    test lint-staged

--- src/pages/board-list/BoardList.tsx
+++ src/pages/board-list/BoardList.tsx
@@ -4,7 +4,7 @@ import { boardPb } from '@/lib/pb/board'
 import { queryKeys } from '@/lib/query'
 
 export default function BoardList() {
-  const { data } = useQuery({
+  useQuery({
     ...queryKeys.boards.all,
     queryFn: boardPb.getList,
   })
