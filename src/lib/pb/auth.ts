661d3869b1baaae30a550d105c6af6897bac7029
blackparadise0407 <blackparadise0407@gmail.com>
2023-04-27 12:08:05 +0700

    fix: allow verified user to sign in

--- src/lib/pb/auth.ts
+++ src/lib/pb/auth.ts
@@ -15,10 +15,16 @@ interface RegisterDto {
 const c = pb.collection('users')
 
 export const authPb = {
-  login(loginDto: LoginDto) {
-    return c.authWithPassword(loginDto.username, loginDto.password, undefined, {
-      verified: true,
-    })
+  async login(loginDto: LoginDto) {
+    const resp = await c.authWithPassword<User>(
+      loginDto.username,
+      loginDto.password,
+    )
+    if (!resp.record.verified) {
+      pb.authStore.clear()
+      throw new Error('Account not verified')
+    }
+    return resp
   },
   async register(registerDto: RegisterDto) {
     const registeredUser = await c.create<User>(registerDto)
